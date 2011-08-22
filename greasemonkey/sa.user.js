a646ef662e9945ab8bca14e72b026367947fbd54
mikecleach <mikecleach@gmail.com>
2011-08-22 11:39:05 -0500

    Initial commit, added SA strip script

---
+++ greasemonkey/sa.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name     My jQuery Test
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
+// @include  http://forums.somethingawful.com/*
+// ==/UserScript==
+
+$("td.icon").empty();
+$("dd.title").remove();
+$("div.oma_pal").remove();
